aa0b91a0f865e5b4d89066c95cfae288a30d4a8c
Serge Merzliakov <smerzlia@gmail.com>
2019-04-03 18:35:13 +1100

    Build working for JavaFx 11.0.2

--- src/main/kotlin/org/epistatic/app/Main.kt
+++ src/main/kotlin/org/epistatic/app/Main.kt
@@ -1,32 +1,26 @@
 package org.epistatic.app
 
-//import javafx.application.Application
-//import javafx.fxml.FXMLLoader
-//import javafx.scene.Parent
-//import javafx.scene.Scene
-//import javafx.stage.Stage
+import javafx.application.Application
+import javafx.fxml.FXMLLoader
+import javafx.scene.Parent
+import javafx.scene.Scene
+import javafx.stage.Stage
 
-//class Main {: Application() {
+class Main: Application() {
 
-//	@Throws(Exception::class)
-//	override fun start(primaryStage: Stage) {
-//		val root = FXMLLoader.load<Parent>(javaClass.getResource("/javafx-app-1.fxml"))
-//		primaryStage.title = "Simple App 1"
-//		primaryStage.scene = Scene(root)
-//		primaryStage.show()
-//	}
+	@Throws(Exception::class)
+	override fun start(primaryStage: Stage) {
+		val root = FXMLLoader.load<Parent>(javaClass.getResource("/javafx-app-1.fxml"))
+		primaryStage.title = "Simple App 1"
+		primaryStage.scene = Scene(root)
+		primaryStage.show()
+	}
 
-
-//	companion object {
-//		@JvmStatic
-//		fun main(args: Array<String>) {
-//			//launch(Main::class.java, *args)
-//
-//		}
-//	}
-//}
-
-
-fun main() {
-	println("Hello")
+	companion object {
+		@JvmStatic
+		fun main(args: Array<String>) {
+			launch(Main::class.java, *args)
+		}
+	}
 }
+
--- src/main/kotlin/org/epistatic/app/controller/Controller.kt
+++ src/main/kotlin/org/epistatic/app/controller/Controller.kt
@@ -1,41 +1,41 @@
 package org.epistatic.app.controller
 
-//import javafx.collections.FXCollections
-//import javafx.fxml.FXML
-//import javafx.scene.control.ListView
-//import javafx.scene.control.TextField
-//import javafx.stage.Stage
+import javafx.collections.FXCollections
+import javafx.fxml.FXML
+import javafx.scene.control.ListView
+import javafx.scene.control.TextField
+import javafx.stage.Stage
 
 class Controller {
 
-//	@FXML lateinit var itemListView: ListView<String>
-//	@FXML lateinit var newItemField: TextField
-//
-//	val model = FXCollections.observableArrayList<String>()
-//
-//	@FXML
-//	fun initialize() {
-//		// Preload some data into model, which is then loaded into list via observable property magic
-//		model.add("Lion")
-//		model.add("Bear")
-//		model.add("Giraffe")
-//		itemListView.items = model
-//	}
-//
-//	@FXML
-//	fun closeApplication() {
-//		val stage = itemListView.scene.window as Stage
-//		stage.close()
-//	}
-//
-//	@FXML
-//	fun addItem() {
-//		val newItem = newItemField.text.trim()
-//
-//		if (newItemField.text.trim().isEmpty())
-//			return
-//
-//		model.add(newItem)
-//
-//	}
+	@FXML lateinit var itemListView: ListView<String>
+	@FXML lateinit var newItemField: TextField
+
+	val model = FXCollections.observableArrayList<String>()
+
+	@FXML
+	fun initialize() {
+		// Preload some data into model, which is then loaded into list via observable property magic
+		model.add("Lion")
+		model.add("Bear")
+		model.add("Giraffe")
+		itemListView.items = model
+	}
+
+	@FXML
+	fun closeApplication() {
+		val stage = itemListView.scene.window as Stage
+		stage.close()
+	}
+
+	@FXML
+	fun addItem() {
+		val newItem = newItemField.text.trim()
+
+		if (newItemField.text.trim().isEmpty())
+			return
+
+		model.add(newItem)
+
+	}
 }
