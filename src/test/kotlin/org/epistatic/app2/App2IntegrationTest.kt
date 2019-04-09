7d4ae919bea74fde2a8794b91b59662230afe26c
SergeQantasDEV <serge.merzliakov@qantasloyalty.com>
2019-04-09 14:06:53 +1000

    tidy up the formatting and removed unused constants

--- src/test/kotlin/org/epistatic/app2/App2IntegrationTest.kt
+++ src/test/kotlin/org/epistatic/app2/App2IntegrationTest.kt
@@ -21,7 +21,6 @@ package org.epistatic.app2
 
 import javafx.fxml.FXMLLoader
 import javafx.scene.Scene
-import javafx.scene.control.ListCell
 import javafx.scene.control.ListView
 import javafx.scene.layout.VBox
 import javafx.stage.Stage
@@ -29,77 +28,74 @@ import org.assertj.core.api.Assertions.assertThat
 import org.epistatic.app2.controller.Controller
 import org.epistatic.app2.model.Person
 import org.epistatic.test.utils.BetterApplicationTest
-import org.junit.Ignore
 import org.junit.Test
-import org.testfx.framework.junit.ApplicationTest
 
 /**
  * Use Junit TestFX to integration test the application UI, so
- * not unit tests per se.
+ * not unit tests, but closer to integration tests.
+ * 
+ * Also no tests for the fileListView yet.
  */
 class App2IntegrationTest : BetterApplicationTest() {
 
-	// Node Ids defined in the FXML file. Is a CSS Id selector
-	// Use these to uniquely identify JavaFX controls for testing
-	companion object {
-		const val FRIEND_LIST_VIEW = "#friendListView"
-		const val PARTY_LIST_VIEW = "#partyListView"
-		const val FILE_LIST_VIEW = "#fileListView"
-		const val CANCEL_BUTTON = "#cancelButton"
+   // Node Ids defined in the FXML file. Is a CSS Id selector
+   // Use these to uniquely identify JavaFX controls for testing
+   companion object {
+      const val FRIEND_LIST_VIEW = "#friendListView"
+      const val PARTY_LIST_VIEW = "#partyListView"
+   }
 
-	}
-
-	private lateinit var controller: Controller
+   private lateinit var controller: Controller
 
 
-	override fun start(stage: Stage?) {
-		// We instantiate a loader directly so we can access the controller for testing
-		val loader = FXMLLoader(javaClass.getResource("/app2/app2.fxml"))
-		val root = loader.load<VBox>()
-		controller = loader.getController()
-		stage?.scene = Scene(root)
-		stage?.show()
-	}
+   override fun start(stage: Stage?) {
+      // We instantiate a loader directly so we can access the controller for testing
+      val loader = FXMLLoader(javaClass.getResource("/app2/app2.fxml"))
+      val root = loader.load<VBox>()
+      controller = loader.getController()
+      stage?.scene = Scene(root)
+      stage?.show()
+   }
 
 
-	@Test
-	fun shouldDragSingleFriendToPartyList() {
+   @Test
+   fun shouldDragSingleFriendToPartyList() {
       val friendCount = controller.friends.size
-      
-		// drag a single friend to party list
-		dragFriendToParty("Andrea")
 
-		val partyList = lookup(PARTY_LIST_VIEW).query<ListView<Person>>()
+      // drag a single friend to party list
+      dragFriendToParty("Andrea")
+
+      val partyList = lookup(PARTY_LIST_VIEW).query<ListView<Person>>()
 
       // check model
       assertThat(controller.friends).hasSize(friendCount - 1)
       assertThat(controller.partyGuests).hasSize(1)
-      
+
       // check view
-		assertThat(partyList.items).hasSize(1)
-		assertThat(partyList.items[0]).hasFieldOrPropertyWithValue("firstName", "Andrea")
-	}
+      assertThat(partyList.items).hasSize(1)
+      assertThat(partyList.items[0]).hasFieldOrPropertyWithValue("firstName", "Andrea")
+   }
 
 
-	@Test
-	fun shouldDragAllFriendsToPartyList() {
-		// drag everyone to party
-		val friendCount = controller.friends.size
+   @Test
+   fun shouldDragAllFriendsToPartyList() {
+      // drag everyone to party
+      val friendCount = controller.friends.size
 
-		// avoid ConcurrentModificationException - copy names into separate list
-		val firstNames = controller.friends.map { it.firstName }
-		firstNames.map { dragFriendToParty(it) }
+      // avoid ConcurrentModificationException - copy names into separate list
+      val firstNames = controller.friends.map { it.firstName }
+      firstNames.map { dragFriendToParty(it) }
 
-		// check model
-		assertThat(controller.friends).isEmpty()
-		assertThat(controller.partyGuests).hasSize(friendCount)
+      // check model
+      assertThat(controller.friends).isEmpty()
+      assertThat(controller.partyGuests).hasSize(friendCount)
 
-		// check view
-		val friendView = lookup(FRIEND_LIST_VIEW).query<ListView<Person>>()
-		assertThat(friendView.items).isEmpty()
-		val partyView = lookup(PARTY_LIST_VIEW).query<ListView<Person>>()
-		assertThat(partyView.items).hasSize(friendCount)
-	}
+      // check view
+      val friendView = lookup(FRIEND_LIST_VIEW).query<ListView<Person>>()
+      assertThat(friendView.items).isEmpty()
+      val partyView = lookup(PARTY_LIST_VIEW).query<ListView<Person>>()
+      assertThat(partyView.items).hasSize(friendCount)
+   }
 
 
    @Test
@@ -108,9 +104,9 @@ class App2IntegrationTest : BetterApplicationTest() {
       val friendCount = controller.friends.size
       val friendView = lookup(FRIEND_LIST_VIEW).query<ListView<Person>>()
       val partyView = lookup(PARTY_LIST_VIEW).query<ListView<Person>>()
-      
+
       dragFriendToParty(friend)
-  
+
       // check model and view after dragging from friendView -> partyView
       assertThat(controller.friends).hasSize(friendCount - 1)
       assertThat(controller.partyGuests).hasSize(1)
@@ -127,17 +123,17 @@ class App2IntegrationTest : BetterApplicationTest() {
       assertThat(friendView.items).hasSize(friendCount)
       assertThat(partyView.items).isEmpty()
    }
-   
-   
-	private fun dragFriendToParty(firstName: String) {
-		val friend = getListViewRowByFirstName<Person>(FRIEND_LIST_VIEW, firstName)
-		val d = drag(friend)
-		d.dropTo(PARTY_LIST_VIEW)
-	}
+
+
+   private fun dragFriendToParty(firstName: String) {
+      val friend = getListViewRowByFirstName<Person>(FRIEND_LIST_VIEW, firstName)
+      val d = drag(friend)
+      d.dropTo(PARTY_LIST_VIEW)
+   }
 
    private fun dragFriendOutOfParty(firstName: String) {
       val friend = getListViewRowByFirstName<Person>(PARTY_LIST_VIEW, firstName)
-      val d = drag(friend)
-      d.dropTo(FRIEND_LIST_VIEW)
+      val dragRobot = drag(friend)
+      dragRobot.dropTo(FRIEND_LIST_VIEW)
    }
 }
