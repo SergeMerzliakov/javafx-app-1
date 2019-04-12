5e7495e6438d08aedd6d9c4301673b60fa3066a2
Serge Merzliakov <smerzlia@gmail.com>
2019-04-12 21:38:12 +1000

    Added Google event bus dependency and all controller skeletons for app3

---
+++ src/main/kotlin/org/epistatic/app3/Main.kt
@@ -0,0 +1,52 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ **/
+
+package org.epistatic.app3
+
+import javafx.application.Application
+import javafx.fxml.FXMLLoader
+import javafx.scene.Parent
+import javafx.scene.Scene
+import javafx.stage.Stage
+
+/**
+ * A Application with multiple controller
+ *
+ *   1) Assigning controllers to FXML at runtime
+ *   2) Multiple Controllers communicating via Events
+ *   3) Dragging Items from the Desktop
+ *   4) Controllers can leverage dependency injection
+ */
+class Main : Application() {
+
+	@Throws(Exception::class)
+	override fun start(primaryStage: Stage) {
+		val root = FXMLLoader.load<Parent>(javaClass.getResource("/app3/app3.fxml"))
+		primaryStage.title = "Multiple Controllers And Events"
+		primaryStage.scene = Scene(root, 650.0, 550.0)
+		primaryStage.show()
+	}
+
+	companion object {
+		@JvmStatic
+		fun main(args: Array<String>) {
+			launch(Main::class.java, *args)
+		}
+	}
+}
---
+++ src/main/kotlin/org/epistatic/app3/controller/ApplicationController.kt
@@ -0,0 +1,41 @@
+package org.epistatic.app3.controller
+
+import javafx.fxml.FXML
+import javafx.scene.control.Button
+import javafx.scene.layout.AnchorPane
+
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ **/
+
+/**
+ * Controller for app3/app3.fxml
+ */
+class ApplicationController {
+
+	@FXML lateinit var exitButton: Button
+	@FXML lateinit var leftPane: AnchorPane
+	@FXML lateinit var topRightPane: AnchorPane
+	@FXML lateinit var bottomRightPane: AnchorPane
+
+
+	@FXML
+	fun initialize(){
+
+	}
+}
