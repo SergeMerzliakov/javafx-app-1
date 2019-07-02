e8691e05fa40055cd9279264410e6c6cb9a8f91c
SergeQantasDEV <serge.merzliakov@qantasloyalty.com>
2019-07-02 15:50:45 +1000

    Module path needs to be libs directories to get all dependencies

--- deps.sh
+++ deps.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-jdeps -s --module-path $JAVA_HOME/jmods:/Library/Java/javafx/12.0.1/jmods --no-recursive --class-path lib/* javafx-app-1-1.0.0.jar
+jdeps -s --module-path $JAVA_HOME/lib:/Library/Java/javafx/12.0.1/lib --no-recursive --class-path installBuild/lib/* installBuild/javafx-app-1-1.0.0.jar
