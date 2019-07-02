c0ccae7e5a7c5937f5eec46c7bdb1038d9cd524f
SergeQantasDEV <serge.merzliakov@qantasloyalty.com>
2019-07-02 15:51:40 +1000

    moved to root folder. Working

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-installBuild/jre/bin/java -cp installBuildjavafx-app-1-1.0.0.jar:lib/* org.epistatic.app3.Main
+installBuild/jre/bin/java -cp installBuild/javafx-app-1-1.0.0.jar:installBuild/lib/* org.epistatic.app3.Main
