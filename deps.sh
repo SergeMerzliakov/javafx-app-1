3c93905a6c57ce4d047514c6856b14e85ce94817
Serge Merzliakov <smerzlia@gmail.com>
2019-07-02 22:22:00 +1000

    fixed for Java12

--- deps.sh
+++ deps.sh
@@ -2,4 +2,4 @@
 export JAVA_HOME=$1
 export JFX_INSTALL=$2
 export JAR_FILE=$3
-$JAVA_HOME/bin/jdeps -s --module-path $JAVA_HOME/lib:$JFX_INSTALL/lib --no-recursive --class-path installBuild/lib/* $JAR_FILE
+$JAVA_HOME/bin/jdeps -s --module-path $JAVA_HOME/lib:$JFX_INSTALL/lib --class-path installBuild/lib/* $JAR_FILE
