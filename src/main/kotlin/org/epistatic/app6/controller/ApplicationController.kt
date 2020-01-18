7497da406cb781f3425c834775701b5d99204593
SergeMerzliakov <serge.merzliakov@qantasloyalty.com>
2020-01-19 10:36:42 +1100

    Fixed dialog messages to show correct field identifier.

--- src/main/kotlin/org/epistatic/app6/controller/ApplicationController.kt
+++ src/main/kotlin/org/epistatic/app6/controller/ApplicationController.kt
@@ -45,10 +45,10 @@ class ApplicationController {
 
    private fun initializeFields() {
       // method 1 - call extension function
-      textFieldExtension.onFocusLost { showDialog("Focus Lost", "textField1 Lost Focus.\n[Called extension function - onFocusLost]") }
+      textFieldExtension.onFocusLost { f->  showDialog("Focus Lost", "${textFieldExtension.id} Lost Focus.\n[Called extension function - onFocusLost]") }
 
       // method 2 - call simple wrapper function
-      EventWrapper.onFocusLost(textFieldWrapper) { showDialog("Focus Lost", "textField2 Lost Focus.\n[Called Wrapper - EventHandler.onFocusLost]") }
+      EventWrapper.onFocusLost(textFieldWrapper) { f -> showDialog("Focus Lost", "${textFieldWrapper.id} Lost Focus.\n[Called Wrapper - EventHandler.onFocusLost]") }
    }
    
    private fun initializeCombo() {
